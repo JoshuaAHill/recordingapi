6a689accacbe238a99a2f58eaf6d14609c2cb2d3
Joshua-Hill <71715134+Joshua-Hill@users.noreply.github.com>
2021-04-16 01:46:37 +0100

    Added Example Blocks for User Testing

--- custom.ts
+++ custom.ts
@@ -4,35 +4,93 @@
  * Read more at https://makecode.microbit.org/blocks/custom
  */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
-
 /**
  * Custom blocks
  */
-//% weight=100 color=#0fbc11 icon=""
-namespace custom {
+//% weight=100 color=#0fbc11 icon=""
+namespace recording {
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * Record a sound clip - max length
      */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
+    //% block Record Soundclip
+    export function record() {
+        // Placeholder for Testing
     }
 
     /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
+     * play back the recorded soundclip - assumes only one
      */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    //% block Playback Soundclip
+    export function playback() {
+        // Placeholder for Testing
     }
+
+    /**
+     * play back the recorded soundclip at a given sample rate
+     */
+    //% block Playback Soundclip at %sampleRate
+    //%
+    export function playbackSampleRate(sampleRate : number = 11025) {
+        // Placeholder for Testing
+    }
+
+    /**
+     * Record a clip of a given length
+     */
+    //% block Record Soundclip for %length
+    //%
+    export function recordLength(length : number = 11025) {
+        // Placeholder for Testing
+    }
+
+    /**
+     * Having a class like this could mean a paradigm shift in C++
+     * where the buffer is returned and not stored within mic recorder itself
+     * i.e. mySoundClip = micRecorder.record(duration:3, sampleRate:11025);
+     */
+    export class Clip{
+        duration : number;
+
+        constructor(duration: number){
+            this.duration = duration;
+        }
+
+        record(){
+
+        }
+        playback(){
+
+        }
+    }
+
+    /**
+     * Create a clip widget
+     */
+    //% blockId=create_clip
+    //% block="create clip of length %duration"
+    //% blockSetVariable=clip
+    //% duration.min=1 duration.max=4 duration.defl=3
+    export function createClipVar(duration : number = 3): Clip {
+        return new Clip(duration);
+    }
+
+    /**
+     * Record a clip variable
+     */
+    //% block Record Soundclip %clip
+    //%
+    export function recordClipVar(clip : Clip) {
+        clip.record();
+    }
+
+    /**
+     * Playback a clip variable
+     */
+    //% block Playback Soundclip %clip
+    //%
+    export function playbackClipVar(clip : Clip) {
+        clip.playback();
+    }
+
+
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+let clip: recording.Clip = null
 basic.forever(function () {
-	
+    clip = recording.createClipVar(4)
+    recording.recordClipVar(clip)
 })
